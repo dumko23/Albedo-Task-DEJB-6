06d241d2174adde440013154896522ce73c057b9
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-08-19 17:10:17 +0300

    Improving webpack config to bundle project

---
+++ App/assets/script.js
@@ -0,0 +1,68 @@
+let currentSlide = 0;
+
+document
+    .getElementById('slide-left')
+    .addEventListener('click', function () {
+        slide('left');
+    })
+    ;
+document
+    .getElementById('slide-right')
+    .addEventListener('click', function () {
+        slide('right');
+    })
+    ;
+slide();
+
+function slide(direction = null) {
+    let slides = ['slide-1', 'slide-2', 'slide-3'];
+    if (direction === 'left') {
+        currentSlide--;
+    } else if (direction === 'right') {
+        currentSlide++;
+    }
+    if (currentSlide === 3) {
+        currentSlide = 0;
+    } else if (currentSlide === -1) {
+        currentSlide = 2;
+    }
+    slides.forEach(element => {
+        document.getElementById(element).style.display = 'none';
+    });
+    document.getElementById(slides[currentSlide]).style.display = 'block';
+}
+
+document
+    .getElementById('burger')
+    .addEventListener('click', function () {
+        toggleMenu(true);
+    })
+    ;
+document
+    .getElementById('xmark')
+    .addEventListener('click', function () {
+        toggleMenu(false);
+    })
+    ;
+window.addEventListener('resize', function () {
+    toggleMenu(false);
+});
+
+toggleMenu(false);
+
+function toggleMenu(toggle = '') {
+    if (toggle === false) {
+        document.getElementById('burger').style.display = 'block';
+        document.getElementById('xmark').style.display = 'none';
+        document.getElementById('nav-div').style.display = 'none';
+    } else if (toggle === true) {
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('xmark').style.display = 'block';
+        document.getElementById('nav-div').style.display = 'block';
+    }
+    let menu = window.getComputedStyle(document.getElementById('menu'));
+    if (menu['display'] === 'none') {
+        console.log(menu['display'])
+        document.getElementById('nav-div').style.display = 'flex';
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
    mode: 'development',
    entry: './App/assets/index.js',
@@ -8,18 +9,32 @@ module.exports = {
       path: path.resolve(__dirname, 'App/assets/dist')
    },
    module: {
-      rules: [{
-         test:/\.(s*)css$/,
-         use: [
-            miniCss.loader,
-            'css-loader',
-            'sass-loader',
-         ]
-      }]
+      rules: [
+         {
+            test: /\.(s*)css$/,
+            use: [
+               miniCss.loader,
+               'css-loader',
+               'sass-loader',
+            ]
+         },
+         {
+            test: /\.html$/i,
+            loader: "html-loader",
+         },
+
+      ]
    },
    plugins: [
       new miniCss({
          filename: 'style.css',
       }),
-   ]
+      new HtmlWebpackPlugin({
+         title: 'Landing Page',
+         filename: 'index.html',
+         hash: true,
+         template: './App/index.html'
+      })
+   ],
+
 };
